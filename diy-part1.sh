fa5a27572f4735af6d1dea6d58fcb465ffd2916f
StartMoney <StartMoney@users.noreply.github.com>
2023-02-01 20:26:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,22 +9,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-
-#1. Modify default IP
-#sed -i 's/192.168.1.1/192.168.66.1/g' openwrt/package/base-files/files/bin/config_generate
-
-#2. 修改密码
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#3.可以修改feeds,但最好放在 diy2中去，用clone目录的方式
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
